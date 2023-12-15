806ee90e75c5b6107ac91460e5ee2d546375fc2c
David Freeman <84056064+sonrai-davo@users.noreply.github.com>
2023-12-15 12:11:26 +1100

    Update etl-companies-appgroups.js
    Updated error handling and console output on loading AppGroups into Apigee X. Other minor console output improvements

--- migrate-edge-to-x/etl-companies-appgroups.js
+++ migrate-edge-to-x/etl-companies-appgroups.js
@@ -7,10 +7,10 @@ const axiosRequest = require('axios');
 const prompt = require('prompt-sync')();
 
 //Prompt for user input to access Apigee Edge Org and Apigee X project.
-const org = prompt('Enter the Apigee Edge Organization name to get the Company records from:')
-const token = prompt('Enter your Apigee Edge OAuth2 token:');
-const apigee_x_project = prompt('Enter the Apigee X Project to load the transformed AppGroups into:')
-const gcp_token = prompt('Enter the GCP OAuth2 token:')
+const org = prompt('Enter the Apigee Edge Organization name to get the Company records from: ')
+const token = prompt('Enter your Apigee Edge OAuth2 token: ');
+const apigee_x_project = prompt('Enter the Apigee X Project to load the transformed AppGroups into: ')
+const gcp_token = prompt('Enter the GCP OAuth2 token: ')
 
 //Extract - get Company records and save them to a local file.
 //Set variables for the API call from user input to get Company records from Apigee Edge.
@@ -24,11 +24,13 @@ function extract(org, token, callback) {
     }
   };
   //Make request to Apigee Edge for all company data and save into local companies.json file
+  console.log("Extracting company records from Apigee Edge ORG " + org)
   try {
     axiosRequest.request(config)
       .then(response => {
-        console.log(response.data);
+        //console.log(response.data);
         fs.writeFileSync('companies.json', JSON.stringify(response.data, null, "\t"))
+        console.log(response.data.company.length + " company records have been saved to companies.json file.")
         callback();
       })
   }
@@ -41,6 +43,7 @@ function extract(org, token, callback) {
 //Process the AppGroups and add members to the AppGroup as "member" and "app_developers"
 async function transform(){
   //Use the data in the JSON file previously written by the extract function.
+  console.log("Transforming company records from companies.json into AppGroups")
   try {
     var dataSet = fs.readFileSync('./companies.json', 'utf8')
     } catch (err) {
@@ -50,6 +53,7 @@ async function transform(){
   //convert dataSet to JSON and place just the company records into a variable
   const company_data = JSON.parse(dataSet);
   const companyRecords = company_data.company
+  console.log(`Number of Companies to convert to AppGroups: ${companyRecords.length}`)
 
   //Create appGroup array shell
   var appGroupArray = {"appGroups":[]};
@@ -82,6 +86,7 @@ async function transform(){
     appGroupArray.appGroups.push(company);
   }
   fs.writeFileSync('./appGroups.json', JSON.stringify(appGroupArray, null, "\t"))
+  console.log("AppGroups are ready to load: ")
   console.log(appGroupArray);
   load(apigee_x_project,gcp_token)
 }
@@ -107,8 +112,9 @@ async function getDevelopers(token, org, company) {
 }
 
 //Load - using appsGroups.json file, load dataset into Apigee X project
-function load(apigee_x_project, gcp_token) {
+async function load(apigee_x_project, gcp_token) {
   //Read appsGroups.json file into variable dataSet
+  console.log("Loading AppGroups from appGroups.json file into Apigee X project " + apigee_x_project)
   try {
   var dataSet = fs.readFileSync('./appGroups.json', 'utf8')
   //console.log(dataSet)
@@ -118,11 +124,11 @@ function load(apigee_x_project, gcp_token) {
 
   //convert dataSet to JSON
   const appGroup_data = JSON.parse(dataSet);
-  console.log(appGroup_data.appGroups)
+  //console.log(appGroup_data.appGroups)
 
   //Create appGroups iteratively
   var numAppGroups = appGroup_data.appGroups.length
-  console.log(numAppGroups)
+  console.log(`Number of AppGroups to migrate: ${numAppGroups}`)
 
   for (var i = 0; i < numAppGroups; i++) {
       let config = {
@@ -137,13 +143,19 @@ function load(apigee_x_project, gcp_token) {
       };
     
       try {
-        axiosRequest.request(config)
-          .then(response => {
-            console.log(response.data);
-          })
-      }
-      catch (error) {
-        console.error(`ERROR: ${error}`)
+        let response = await axiosRequest.request(config);
+        console.log(response.status + ' ' + response.statusText + ' - ' + '"' + response.data.name + '"' + ' in Apigee X project: ' + apigee_x_project)
+      } catch (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code that falls out of the range of 2xx
+          console.log('ERROR: ' + error.response.status + ' - ' + error.response.data.error.message)
+        } else if (error.request) {
+          // The request was made but no response was received `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
       }
   }
 }
